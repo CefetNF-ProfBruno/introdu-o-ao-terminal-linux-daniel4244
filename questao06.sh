3a049baa8f97dfc101d7ce5c3f2635ed1ae13d9e
daniel <danielfcosta.com.br>
2025-05-11 20:53:48 -0300

    questao06 feita

--- questao06.sh
+++ questao06.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+cd ~
+mv mais_uma_pasta/joao mais_uma_pasta/temp
+mv mais_uma_pasta/maria mais_uma_pasta/joao
+mv mais_uma_pasta/temp mais_uma_pasta/maria
+
 ### Nao apague a linha acima! ###
